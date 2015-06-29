0e6a2093c9abefb1ee6bf196dafdb76062a9c217
murata <h_murata@binfo-lab.net>
2015-06-29 18:44:26 +0900

    jsosero.electron

---
+++ index.js
@@ -0,0 +1,16 @@
+$(function() {
+    var osero = new Osero('bord', 8, 8);
+
+    var $message = $('#message');
+    osero.onTurnStart = function(e) {
+        var message = (e.stone === osero.blackStone ? '黒' : '白') + 'の番です。';
+        $message.text(message);
+    };
+
+    osero.onFinish = function(e) {
+        var message = e.blackCount + '対' + e.whiteCount + 'で' + (e.blackCount > e.whiteCount ? '黒' : '白') + 'の勝ちです';
+        $message.text(message);
+    };
+
+    osero.start();
+});
